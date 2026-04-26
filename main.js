9691ff0af452103cf923cbf0c1155193d1f6a63b
Dimitri <dimitrifortini91@gmail.com>
2026-04-26 16:49:41 +0200

    Eseguito selfwork oggetti 1

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// SELFWORK OGGETTI 1
+
+let persona = {
+    name: "Dimitri",
+    surname: "Fortini",
+    age:34,
+    
+    
+};
+
+console.log(`Ciao, sono ${persona.name} ${persona.surname} ed ho ${persona.age} anni`);
